1a309606be8259a6f72a4a29a769f3c15371c764
¨David <daopz23@gmail.com>
2019-10-26 17:26:34 -0300

    Add trigger

--- generate_job.groovy
+++ generate_job.groovy
@@ -3,6 +3,9 @@ job('generated-job') {
     scm {
         git('https://git:git@github.com/beerkeeper/python-ip-script', 'master')
     }
+    triggers {
+        scm('*/2 * * * *')
+    }
     steps {
       	shell('''
 pip install -r requirements.txt
